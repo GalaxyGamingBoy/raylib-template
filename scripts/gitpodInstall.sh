7c0d89d22034dbba8ae6904bd502696a3f65bf84
GalaxyGamingBoy <xrteach@gmail.com>
2023-05-26 18:25:19 +0000

    feat: Basic

---
+++ scripts/gitpodInstall.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install -y libgl1-mesa-dev
+sudo apt-get install -y libxi-dev
+sudo apt-get install -y libxcursor-dev
+sudo apt-get install -y libxinerama-dev
+sudo apt-get install -y libxrandr-dev
